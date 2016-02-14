7f0e8fcd9c750823eaa34bda03d1fdd5849d3af6
Brian Wynn <brian.wynn83@gmail.com>
2016-02-13 23:09:18 -0800

    Updated profile html and got user service set up.

--- public/js/controllers/profileCtrl.js
+++ public/js/controllers/profileCtrl.js
@@ -1,5 +1,15 @@
 angular.module('profileCtrl', [])
-  .controller('ProfileController', ['$scope', function($scope) {
-    
+  .controller('ProfileController', ['$scope', 'User', function($scope, User) {
+
     $scope.hello = "Welcome! You made it to the Profile controller !";
+
+    // Get user data
+    User.get().then(function(user) {
+
+      // set user details
+      $scope.user = user.data;
+
+      console.log($scope.user);
+    });
+
   }]);
--- public/js/services/userService.js
+++ public/js/services/userService.js
@@ -1,1 +1,17 @@
 angular.module('UserService', [])
+  .factory('User', ['$http', function($http) {
+    return {
+      get: function() {
+        return $http.get('/api/profile');
+      },
+      post: function(userData) {
+        return $http.post('/api/profile', userData);
+      },
+      put: function(userData) {
+        return $http.put('/api/profile', userData);
+      },
+      delete: function() {
+        return $http.delete('/api/profile');
+      }
+    };
+  }]);
