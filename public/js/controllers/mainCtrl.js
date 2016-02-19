02bce0ccb780cc0399a1dd152e2a19d37acb7dcb
Brian Wynn <brian.wynn83@gmail.com>
2016-02-19 00:10:05 -0800

    Working on styles mostly tonight. Got desktop navigation items sorted out. Began working on form elements as well.

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,13 +1,31 @@
 angular.module('mainCtrl', [])
   .controller('MainController', ['$scope', '$rootScope', function($scope, $rootScope) {
+
     // handle communication between scopes
     $rootScope.$on('handleEmit', function(event, args) {
       // 1console.log(args.car);
       $rootScope.car = args.car;
     });
 
+    // send avg value
     $rootScope.$on('carAvgEmit', function(event, args) {
       $rootScope.avg = args.avg;
-    })
+    });
+
+    // set controller states
+    $scope.closed = true;
+
+    // navigation controls
+    $scope.menuBtn = function() {
+      if ($scope.closed == false) {
+        $scope.closed = true;
+      }
+      else if ($scope.closed == true) {
+        $scope.closed = false;
+      }
+      else {
+        return console.log("somethings up with the menu toggle");
+      }
+    };
 
   }]);
